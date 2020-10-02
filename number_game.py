0dd50c99d0856919bbb7e01cb418ea6003e58dea
John Keatings <keatingsjohn1@gmail.com>
2020-10-01 23:10:58 -0400

    added number_game.py

---
+++ number_game.py
@@ -0,0 +1,53 @@
+from random import randint
+import sys
+
+active = True
+
+def play_message():
+    message = input('Would you like to play guess the number? (y/n) ')
+    if message == 'y':
+        number_game()
+    elif message == 'n':
+        sys.exit()
+    else:
+        print('Could not render request, goodbye.')
+        sys.exit()
+
+def play_again():
+    message = input('Would you like to play again? (y/n) ')
+    if message == 'y':
+        number_game()
+    elif message == 'n':
+        print('Thanks for playing!')
+        sys.exit()
+    else: 
+        print('Could not render request, goodbye.')
+        sys.exit() 
+
+def number_game():
+    turn = 0
+    random_number = randint(1, 21)
+
+    while active:
+        user_guess = int(input('Please pick a number between 1 and 20: '))
+
+        if turn <= 4:
+            if user_guess == random_number:
+                print('You are correct!')  
+                play_again()
+            elif user_guess < random_number:
+                print('Too low, guess again.')
+                print(user_guess)
+                turn += 1
+            elif user_guess > random_number:
+                print('Too high, guess again.')
+                print(user_guess)
+                turn += 1
+        else: 
+            print('Sorry, you have lost!')
+            play_again()
+
+play_message()
+number_game()
+
+
